36d99a193cf97b9b91734ce8b71ce7f02b871ef8
Sead <you@example.com>
2021-02-05 04:56:42 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const upButton = document.querySelector(".up");
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if(scroll_position > 600) {
+        upButton.style.right = '10px';
+    } else{
+        upButton.style.right = '-100px';
+    }
+})
