1dec7b424f75e7de6ace17d9dce519ca83c9acb8
Sead <you@example.com>
2021-02-08 15:11:03 +0100

    Commit

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
+const menuIcon = document.querySelector('.toggle');
+const menu = document.querySelector('.menu ');
+const menuList = document.querySelector('.menu, li, a ');
 const upButton = document.querySelector(".up");
+
+menuIcon.addEventListener('click', () => {
+menu.classList.toggle('change');
+menuIcon.classList.toggle('change');
+})
+
+menuList.addEventListener('click', () => {
+    menu.classList.toggle('change');
+    menuIcon.classList.toggle('change');
+})
 
 document.addEventListener('scroll', () => {
     var scroll_position = window.scrollY;
